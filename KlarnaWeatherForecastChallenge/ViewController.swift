dd3af0f4e3889e305beadcffc3c01fc3c0f74f5f
Mehmet Can Seyhan <mehmet.can.seyhan1@gmail.com>
2019-07-04 17:58:04 +0200

    Initial Commit

---
+++ KlarnaWeatherForecastChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KlarnaWeatherForecastChallenge
+//
+//  Created by Mehmet Can Seyhan on 2019-07-04.
+//  Copyright © 2019 Mehmet Can Seyhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
